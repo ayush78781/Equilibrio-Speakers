ca244d806161fdbbcb46b2faea99f5cff485482b
ayush78781 <ayush70781@gmail.com>
2022-02-01 12:56:59 +0530

    connected database

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/speakersDB",{
-    useNewUrlParser : true,
-    useUnifiedTopology : true,
-    // useCreateIndex : true
-})
-.then(()=>{
-    console.log("connection sucsussfull");
-})
-.catch((e)=>{
-    console.log(e);
-})
+mongoose.connect(process.env.DATABASE, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex : true
+    })
+    .then(() => {
+        console.log("connection sucsussfull");
+    })
+    .catch((e) => {
+        console.log(e);
+    })
